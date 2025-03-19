205051c7b979504e83080d959c1475675ba3bfe8
xinyihu3721 <106988073+xinyihu3721@users.noreply.github.com>
2025-03-19 23:27:03 +0000

    Deploying to gh-pages from @ xinyihu3721/xinyihu3721.github.io@b0563cd362ab8689d64d009ad58915fc2386e530 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -40,14 +40,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%78%68%75%30%37@%62%75.%65%64%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/xinyihu3721", "_blank");
         },
       },{
         id: 'social-rss',
@@ -64,11 +64,11 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-x',
+        title: 'X',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://twitter.com/Xhu0926Xinyi", "_blank");
         },
       },{
       id: 'light-theme',
