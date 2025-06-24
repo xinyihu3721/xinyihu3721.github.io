416cfceadd3d9ad35daf0afb478ef7083758d3f8
Xinyi Hu <selenahu@MacBook-Pro-84.local>
2025-06-23 21:40:30 -0400

    null2

--- _scripts/search.liquid.js
+++ _scripts/search.liquid.js
@@ -209,10 +209,6 @@ ninja.data = [
           {%- assign social_id = "social-researchgate" -%}
           {%- assign social_title = "ResearchGate" -%}
           {%- capture social_url %}"https://www.researchgate.net/profile/{{ social[1] }}/"{% endcapture -%}
-        {%- when "rss_icon" -%}
-          {%- assign social_id = "social-rss" -%}
-          {%- assign social_title = "RSS Feed" -%}
-          {%- capture social_url %}"{{ site.baseurl }}/feed.xml"{% endcapture -%}
         {%- when "scholar_userid" -%}
           {%- assign social_id = "social-scholar" -%}
           {%- assign social_title = "Google Scholar" -%}
