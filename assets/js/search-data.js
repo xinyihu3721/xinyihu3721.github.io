938921bb06a14c91875b2f7184df22927a8d0ccf
xinyihu3721 <106988073+xinyihu3721@users.noreply.github.com>
2025-06-24 01:38:34 +0000

    Deploying to gh-pages from @ xinyihu3721/xinyihu3721.github.io@7e3b422d66f031a1b6906a129e3fd968f2ed9a41 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -64,13 +64,6 @@ ninja.data = [{
           window.open("https://orcid.org/0009-0002-8116-2564", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
